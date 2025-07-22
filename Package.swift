347fc932b7ffe65cd88bb8f0a77599af821b29d3
Bilal <yyybilal@gmail.com>
2025-07-22 15:59:46 +0100

    Adds FluentPostgresDriver dependency
    Adds dependencies for FluentPostgresDriver, PostgresKit, and PostgresNIO to enable PostgreSQL database support.

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
     ],
     targets: [
         .target(
@@ -17,13 +18,16 @@ let package = Package(
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
             ]
         ),
         .testTarget(
             name: "VaporRateLimiterTests",
             dependencies: [
                 .target(name: "VaporRateLimiter"),
-                .product(name: "VaporTesting", package: "vapor")
+                .product(name: "VaporTesting", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
             ],
         ),
     ]
